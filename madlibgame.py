87e86dff73c0ea9e63c1dd23f4c518ac2f417813
nishanthini07 <141612486+nishanthini07@users.noreply.github.com>
2024-09-20 21:52:46 +0530

    Add files via upload

---
+++ madlibgame.py
@@ -0,0 +1,12 @@
+#madlibs game
+
+adjective1 = input("enter an adjective (description): ")
+noun1 = input("enter a noun (person, place or thing): ")
+verb1 = input("enter a verb (end with -ing): ")
+adjective2 = input("enter an adjective (description): ")
+adjective3 = input("enter an adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo")
+print(f"In an exhibit I saw a {noun1}")
+print(f"{noun1} was {adjective2} & {verb1}")
+print(f"I was {adjective3}!")
